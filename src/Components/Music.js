323575cef03a5da1fa6596ed3c9d74e030c6681c
Sam Parry <samparrymusic@gmail.com>
2021-01-20 17:27:28 +0000

    music title

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -7,7 +7,7 @@ const Music = () => {
   return (
     <div>
       <section id="music" class="music container-fluid">
-        <h1 class="title music-title text-center">sam the musician</h1>
+        <h1 class="musicTitle music-title text-center">sam the musician</h1>
         <div class="cover">
           <div class="content text-center">
             <div class="websites row flex-wrap">
